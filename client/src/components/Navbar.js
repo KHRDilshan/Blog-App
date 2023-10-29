df212a05f0038aa70ade1d0a84ddba9ecaefac44
KHRDilshan <127587609+KHRDilshan@users.noreply.github.com>
2023-10-29 14:02:47 +0530

    update

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,12 +1,40 @@
 import React from 'react'
 import './navbar.css';
+import Logo from '../image/logo.png'
+import {Link} from 'react-router-dom'
 
 function Navbar() {
   return (
     <div className='navbar' >
       <div className='nav' >
-        <div className='logo'>Logo</div>
-        <div className='links'>Links</div>
+        <div className='logo'>
+          <img src={Logo} alt="" />
+        </div>
+        <div className='links'>
+          <Link className='link' to='/?cat=art'>
+            <h6>ART</h6>
+          </Link>
+          <Link className='link' to='/?cat=science'>
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className='link' to='/?cat=technology'>
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className='link' to='/?cat=cinema'>
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className='link' to='/?cat=design'>
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className='link' to='/?cat=food'>
+            <h6>FOOD</h6>
+          </Link>
+          <span className='name'>Ravin</span>
+          <span className='logout'>Logout</span>
+          <span className='write'>
+            <Link to="/write" className='link'>Write</Link>
+          </span>
+        </div>
       </div>
       
     </div>
